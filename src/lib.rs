1b9c0645bfd479762710ecf8cb69524d83df90b3
jianshu93 <jansuechao@gmail.com>
2025-09-27 15:10:49 -0700

    passes and extra seed

--- src/lib.rs
+++ src/lib.rs
@@ -512,7 +512,7 @@ fn build_vamana_graph<D: Distance<f32> + Copy + Sync>(
 
     // Refinement passes
     const PASSES: usize = 1;
-    const EXTRA_SEEDS: usize = 3;
+    const EXTRA_SEEDS: usize = 2;
 
     let mut rng = thread_rng();
     for _pass in 0..PASSES {
